c6b8f668366a7302bf1be53005edb196e360bfed
MariaRacho <maria.racho@outlook.com>
2025-02-01 15:58:15 +0100

    New code

---
+++ Models/Guest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace RestaurangBokningSystem.Models;
+
+public partial class Guest
+{
+    public int GuestsId { get; set; }
+
+    public string Namn { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Telefonnummer { get; set; } = null!;
+}
